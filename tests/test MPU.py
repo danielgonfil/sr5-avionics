a4e85a497e25156292c35844f9a428ba743848b3
Daniel Gonzalez Filipov <daniel.gonfil@gmail.com>
2024-02-19 12:39:22 +0100

    main

--- tests/test MPU.py
+++ tests/test MPU.py
@@ -3,10 +3,10 @@ import utime
 import mpu
 
 i2c = I2C(0, scl=Pin(21), sda=Pin(20), freq=400000)
-mpu = mpu.MPU(i2c)
+MPU = mpu.MPU(i2c)
  
 while True:
-    data = mpu.read()
+    data = MPU.read()
     print("Temperature: {:.2f} oC".format(data['temp']))
     print("Acceleration: X: {:.2f}, Y: {:.2f}, Z: {:.2f} g"
           .format(data["ax"], data['ay'], data['az']))
